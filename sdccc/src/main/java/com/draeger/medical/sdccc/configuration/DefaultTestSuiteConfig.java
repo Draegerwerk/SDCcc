c8cdafeeae044ae2164ad9861970c7f1348ee279
Björn Engelmann <bjoern.engelmann.contractor@draeger.com>
2023-04-25 10:23:40 +0200

    Moved Binding into DefaultTestSuiteConfig.

--- sdccc/src/main/java/com/draeger/medical/sdccc/configuration/DefaultTestSuiteConfig.java
+++ sdccc/src/main/java/com/draeger/medical/sdccc/configuration/DefaultTestSuiteConfig.java
@@ -35,6 +35,8 @@ public class DefaultTestSuiteConfig extends AbstractConfigurationModule {
         bind(TestSuiteConfig.GRAPHICAL_POPUPS, Boolean.class, true);
 
         bind(TestSuiteConfig.TEST_EXECUTION_LOGGING, Boolean.class, false);
+
+        bind(TestSuiteConfig.SUMMARIZE_MESSAGE_ENCODING_ERRORS, Boolean.class, true);
     }
 
     void configureTLS() {
