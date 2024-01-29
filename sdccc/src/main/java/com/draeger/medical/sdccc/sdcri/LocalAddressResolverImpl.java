ebc36ab17eff28e6c137e8ae4d95b3a3c96c189a
maximilianpilz <104556470+maximilianpilz@users.noreply.github.com>
2024-01-29 11:42:51 +0100

    add missing license

--- sdccc/src/main/java/com/draeger/medical/sdccc/sdcri/LocalAddressResolverImpl.java
+++ sdccc/src/main/java/com/draeger/medical/sdccc/sdcri/LocalAddressResolverImpl.java
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the MIT License.
+ * Copyright (c) 2023 Draegerwerk AG & Co. KGaA.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 package com.draeger.medical.sdccc.sdcri;
 
 import com.draeger.medical.sdccc.configuration.TestSuiteConfig;
