4d84e9ea97fb48cb5a9c7c2d1530f47734cf3a10
Malte Grebe-Lüth <104556762+belagertem@users.noreply.github.com>
2024-04-09 11:55:53 +0200

    Add Kotlin support (#153)
    Added kotlin, detekt and konsist.
    
    # Checklist
    
    The following aspects have been respected by the author of this pull
    request, confirmed by both pull request assignee **and** reviewer:
    
    * Adherence to coding conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Adherence to javadoc conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Changelog update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * README update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * config update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * SDCcc executable ran against a test device (if necessary)
      * [x] Pull Request Assignee
      * [x] Reviewer

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- support for Kotlin
+
 ### Changed
 
 - sdc-ri version to 6.0.0-SNAPSHOT
---
+++ sdccc/src/test/java/com/draeger/medical/sdccc/architecture/KonsistArchitecturalRules.kt
@@ -0,0 +1,47 @@
+package com.draeger.medical.sdccc.architecture
+
+import com.draeger.medical.sdccc.tests.annotations.TestDescription
+import com.draeger.medical.sdccc.tests.annotations.TestIdentifier
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withAnnotationOf
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.Test
+
+/**
+ * Checks the listed architectural rules for Kotlin.
+ */
+class KonsistArchitecturalRules {
+
+    @Test
+    fun `requirement test classes in module 'sdccc' should end with 'Test'`() {
+        checkNameOfRequirementTestClasses("sdccc")
+    }
+
+    @Test
+    fun `all requirement tests in module 'sdccc' should have annotations 'TestIdentifier' and 'TestDescription'`() {
+        checkAnnotationsOfRequirementTests("sdccc")
+    }
+
+    companion object {
+
+        fun checkNameOfRequirementTestClasses(moduleName: String, sourceSet: String = "main") {
+            Konsist.scopeFromProject(moduleName = moduleName, sourceSetName = sourceSet).classes()
+                .filter { it.resideInPackage("..direct..") || it.resideInPackage("..invariant..") }
+                .assertTrue {
+                    it.name.endsWith("Test")
+                }
+        }
+
+        fun checkAnnotationsOfRequirementTests(
+            moduleName: String,
+            sourceSet: String = "main"
+        ) {
+            Konsist.scopeFromProject(moduleName = moduleName, sourceSetName = sourceSet).functions()
+                .filter { it.resideInPackage("..direct..") || it.resideInPackage("..invariant..") }
+                .withAnnotationOf(org.junit.jupiter.api.Test::class)
+                .assertTrue {
+                    it.hasAnnotationOf(TestIdentifier::class) && it.hasAnnotationOf(TestDescription::class)
+                }
+        }
+    }
+}
