6a1a1a9ab5c89a2ce212f0104fecf7c8aeba8085
Malte Grebe-Lüth <malte.grebe-lueth@draeger.com>
2024-07-03 10:07:37 +0200

    make TestParameterConfig constructor public

--- sdccc/src/main/java/com/draeger/medical/sdccc/configuration/TestParameterConfig.java
+++ sdccc/src/main/java/com/draeger/medical/sdccc/configuration/TestParameterConfig.java
@@ -5,12 +5,10 @@ package com.draeger.medical.sdccc.configuration;
  *
  * @see DefaultTestParameterConfig
  */
-public final class TestParameterConfig {
+public class TestParameterConfig {
     /*
      * Test parameter configuration
      */
     private static final String TEST_PARAMETER = "TestParameter.";
     public static final String BICEPS_547_TIME_INTERVAL = TEST_PARAMETER + "Biceps547TimeInterval";
-
-    private TestParameterConfig() {}
 }
