4618ba7a5ae9e16114a0402f7b8c0849a8345bf6
Deichmann, Jan Lukas <janlukas.deichmann@draeger.com>
2024-07-23 14:40:26 +0200

    Add/update some license headers

--- sdccc/src/main/java/com/draeger/medical/sdccc/manipulation/precondition/ObservingPreconditionMdibObserver.kt
+++ sdccc/src/main/java/com/draeger/medical/sdccc/manipulation/precondition/ObservingPreconditionMdibObserver.kt
@@ -1,3 +1,10 @@
+/*
+ * This Source Code Form is subject to the terms of the MIT License.
+ * Copyright (c) 2024 Draegerwerk AG & Co. KGaA.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
 package com.draeger.medical.sdccc.manipulation.precondition
 
 import com.draeger.medical.sdccc.manipulation.precondition.PreconditionChange.Companion.from
