822d3cb461c1583b053b31d48831fdeee7d6977e
midttuna <augustmagnus.midttun@draeger.com>
2024-08-07 21:37:11 +0300

    turn NullableToStringCall back on

--- sdccc/src/main/java/com/draeger/medical/sdccc/manipulation/precondition/PreconditionRegistry.kt
+++ sdccc/src/main/java/com/draeger/medical/sdccc/manipulation/precondition/PreconditionRegistry.kt
@@ -82,7 +82,8 @@ class PreconditionRegistry @Inject internal constructor(private val injector: In
      */
     fun registerObservingPrecondition(precondition: KClass<out ObservingPreconditionFactory<*>>) {
         val factoryInstance = checkNotNull(precondition.objectInstance) {
-            "Factory class ${precondition.simpleName} does not provide an object instance. Ensure it is a Companion."
+            "Factory class ${precondition.simpleName ?: "null"} does not provide an object instance. " +
+                "Ensure it is a Companion."
         }
         val instance = factoryInstance.create(injector)
         if (!preconditions.contains(instance)) {
