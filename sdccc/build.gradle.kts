4b8639e99456db1bb1af9cc9a8d43afc484df785
midttuna <augustmagnus.midttun@draeger.com>
2024-12-03 12:31:27 +0200

    make writeable

--- sdccc/build.gradle.kts
+++ sdccc/build.gradle.kts
@@ -103,7 +103,6 @@ tasks.check {
     dependsOn(detektTask)
     dependsOn("spotbugsMain")
     dependsOn("spotbugsTest")
-    dependsOn("downloadAndUnpackJre")
 }
 
 tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
@@ -138,6 +137,13 @@ tasks.register<de.undercouch.gradle.tasks.download.Download>("downloadJre") {
 }
 
 tasks.register<Copy>("unpackJre") {
+    doFirst {
+        file("${layout.buildDirectory.get().asFile}/${jreBasePath}").walk().forEach { file ->
+            if (!file.setWritable(true)) {
+                println("Failed to set writable permission for ${file.absolutePath}")
+            }
+        }
+    }
     dependsOn("downloadJre")
     from(zipTree(file("${layout.buildDirectory.get().asFile}/${jreBasePath}/${jreDownloadFileName}")))
     into("${layout.buildDirectory.get().asFile}/${jreBasePath}")
