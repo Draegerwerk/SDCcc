57a62a15f5ec9bb7273ff21d2ea3886d8273ede8
Malte Grebe-Lüth <104556762+belagertem@users.noreply.github.com>
2025-01-27 09:19:14 +0100

    Add fix for the sequence id ordering problem (#233)
    Add the fix for the issue
    [218](https://github.com/Draegerwerk/SDCcc/issues/218) from the [commit
    ](https://github.com/belagertem/SDCcc/commit/729356b6eb639ad978e14ae9283cf2284e2f76cc)
    from the main branch.
    Fix other queries in MessageStorage that require ordering.
    
    # Checklist
    
    The following aspects have been respected by the author of this pull
    request, confirmed by both pull request assignee **and** reviewer:
    
    * Adherence to coding conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Adherence to javadoc conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Changelog update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * README update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * config update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * SDCcc executable ran against a test device (if necessary)
      * [x] Pull Request Assignee
      * [x] Reviewer
    
    ---------
    
    Signed-off-by: Malte Grebe-Lüth <malte.grebe-lueth@draeger.com>
    Co-authored-by: Björn <bjoern.engelmann.contractor@draeger.com>
    Co-authored-by: midttuna <162321757+midttuna@users.noreply.github.com>

---
+++ sdccc/src/main/java/com/draeger/medical/sdccc/messages/OrderedStreamIterator.kt
@@ -0,0 +1,45 @@
+/*
+ * This Source Code Form is subject to the terms of the MIT License.
+ * Copyright (c) 2024 Draegerwerk AG & Co. KGaA.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.draeger.medical.sdccc.messages
+
+import org.hibernate.query.spi.CloseableIterator
+import org.hibernate.query.spi.ScrollableResultsImplementor
+
+/**
+ * Iterator to be used to create Streams with the ORDERED characteristic from ScrollableResults.
+ */
+class OrderedStreamIterator<T>(private val results: ScrollableResultsImplementor) : CloseableIterator<T> {
+
+    override fun close() {
+        results.close()
+    }
+
+    override fun remove() {
+        throw UnsupportedOperationException(
+            "this stream does not support the" +
+                " remove operation"
+        )
+    }
+
+    override fun hasNext(): Boolean {
+        if (results.isClosed) {
+            return false
+        }
+        return results.next()
+    }
+
+    override fun next(): T {
+        val element = results.get()
+        @Suppress("UNCHECKED_CAST")
+        return if (element.size == 1) {
+            element[0]
+        } else {
+            element
+        } as T
+    }
+}
