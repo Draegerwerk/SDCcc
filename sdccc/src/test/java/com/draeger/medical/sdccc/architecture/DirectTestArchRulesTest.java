4c4b8df0302f5597101d956694b30673b4f23704
Maximilian Pilz <maximilian.pilz.git@gmail.com>
2025-03-10 15:22:47 +0100

    Split arch rules test and change GLUE.R0036 example config value (#243)
    Split the arch rules test into two tests so that the one requiring
    direct test cases is split and optional to inherit from when inheriting
    the others. Additonally change the GLUE.R0036 example config value from
    true to false.
    
    # Checklist
    
    The following aspects have been respected by the author of this pull
    request, confirmed by both pull request assignee **and** reviewer:
    
    * Adherence to coding conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Adherence to javadoc conventions
      * [x] Pull Request Assignee
      * [x] Reviewer
    * Changelog update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * README update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * config update (necessity checked and entry added or not added
    respectively)
      * [x] Pull Request Assignee
      * [x] Reviewer
    * SDCcc executable ran against a test device (if necessary)
      * [x] Pull Request Assignee
      * [x] Reviewer

---
+++ sdccc/src/test/java/com/draeger/medical/sdccc/architecture/DirectTestArchRulesTest.java
@@ -0,0 +1,44 @@
+/*
+ * This Source Code Form is subject to the terms of the MIT License.
+ * Copyright (c) 2025 Draegerwerk AG & Co. KGaA.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.draeger.medical.sdccc.architecture;
+
+import static com.tngtech.archunit.core.domain.JavaAccess.Predicates.target;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.assignableTo;
+import static com.tngtech.archunit.core.domain.properties.HasName.Predicates.nameMatching;
+import static com.tngtech.archunit.core.domain.properties.HasOwner.Predicates.With.owner;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.draeger.medical.sdccc.tests.util.ImpliedValueUtil;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+/**
+ * Checks the listed Architectural Rules.
+ */
+@AnalyzeClasses(
+        packages = "com.draeger.medical.sdccc",
+        importOptions = {ImportOption.DoNotIncludeTests.class})
+public class DirectTestArchRulesTest {
+
+    @ArchTest
+    private static final ArchRule CHECK_DIRECT_TESTS_INITIAL_IMPLIED_VERSIONS = noClasses()
+            .that()
+            .resideInAPackage("..direct..")
+            .should()
+            .accessTargetWhere(
+                    target(owner(assignableTo(ImpliedValueUtil.class))).and(nameMatching("getDescriptorVersion")))
+            .orShould()
+            .accessTargetWhere(
+                    target(owner(assignableTo(ImpliedValueUtil.class))).and(nameMatching("getStateVersion")))
+            .orShould()
+            .accessTargetWhere(
+                    target(owner(assignableTo(ImpliedValueUtil.class))).and(nameMatching("getStateDescriptorVersion")))
+            .because("Initial implied value can not be reliable checked in direct tests");
+}
